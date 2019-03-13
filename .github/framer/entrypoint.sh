d44a357645917ead535b8aed30b47a78f68c9910
Patrick Camacho <patrick@framer.com>
2019-03-13 15:08:04 +0100

    fix(Workflow): Avoid continuing when errors or undefined variables are present

--- .github/framer/entrypoint.sh
+++ .github/framer/entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-# FRAMER_TOKEN should already be set
+# Avoid continuing when errors or undefined variables are present
+set -eu
+
+# FRAMER_TOKEN env token should already be set
 sh -c "framer $*"
